0640bc0a2ec902171638838ef9629eaca048afd0
Xyyzzzzzz <71469608+MeinanWang@users.noreply.github.com>
2020-11-27 23:19:33 -0800

    Updates

---
+++ precache-manifest.29282399527e86504a075acc306cb23a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001c5fec7626eee405be3f9686782a6d",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "fc80268ccee062fc87c3",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.13c1de33.chunk.css"
+  },
+  {
+    "revision": "b24e61c9310e8d446e94",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.2f90cd53.chunk.js"
+  },
+  {
+    "revision": "fc80268ccee062fc87c3",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.0dfefe54.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "1108ac06e30e8a3413f30a16d099faa4",
+    "url": "/seainfo6150-final-project-webapp/static/media/error.1108ac06.jpg"
+  },
+  {
+    "revision": "9ccbf092e92210bcfe0d54298f45805b",
+    "url": "/seainfo6150-final-project-webapp/static/media/pageHeaderBanner.9ccbf092.jpg"
+  }
+]);
